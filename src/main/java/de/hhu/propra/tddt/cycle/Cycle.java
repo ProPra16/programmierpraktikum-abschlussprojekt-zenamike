a74b4440af56c1730415a60e521b49154e68caa1
Kevin Dreyer <schiggy69@gmail.com>
2016-07-05 21:02:39 +0200

    added the testPhase method

--- src/main/java/de/hhu/propra/tddt/cycle/Cycle.java
+++ src/main/java/de/hhu/propra/tddt/cycle/Cycle.java
@@ -5,6 +5,16 @@ package de.hhu.propra.tddt.cycle;
  */
 
 
+import de.hhu.propra.tddt.util.classnameparser.ClassNameParser;
+import de.hhu.propra.tddt.util.classnameparser.ClassNameParserException;
+import vk.core.api.CompilationUnit;
+import vk.core.api.CompilerFactory;
+import vk.core.api.CompilerResult;
+import vk.core.internal.InternalCompiler;
+
+
+import java.util.LinkedList;
+
 /*********************************************************
  * Class: Cycle
  * <p>
@@ -16,26 +26,73 @@ package de.hhu.propra.tddt.cycle;
  ********************************************************/
 public class Cycle {
 
+    public int listPosition = 0;
+    public LinkedList<CycleEnum> cycleList = new LinkedList<CycleEnum>();
+    CycleEnum phase = CycleEnum.TEST;
+
+
     /*********************************************************
      * Method: startCycle
      * <p>
-     * Start a new cycle, probably loops itselfs as long as the user wishes to
-     * exercise. probably will need additional help functions this class will
-     * communicate with the javaFX part of the program
+     * Start a new cycle
      *
      * @return: void
      ***************************************************************/
 
     public void startCycle() {
+        cycleList.add(CycleEnum.TEST);
+        cycleList.add(CycleEnum.CODE);
+        cycleList.add(CycleEnum.REFACTOR);
 
+        // TODO: 05.07.2016 Ask Zeljiko for the fourth time why a linked list is useful.
+
+        }
+
+    /**
+     * Method: testPhase
+     * <p>
+     * Task: Method that checks if the compile button is clicked, if the right phase was
+     * given to the method. If it got the right phase, the method compiles the test and if
+     * there is only one failed test or the code doesn't compile then the method sets the phase to
+     * the CODE phase.
+     *
+     * @param code String code is the whole code the user typed into the textbox
+     * @param currentPhase CycleEnum currentPhase gives information to the method with which phase
+     *                     the user is currently working. More a failsafe check for the backend
+     *                     processes than for the user.
+     */
+
+    public CycleEnum testPhase(String code, CycleEnum currentPhase)throws ClassNameParserException {
+
+        if (currentPhase.equals(CycleEnum.TEST)) {
+            listPosition = 0;
+            boolean isATest = true;
+            String className = ClassNameParser.getClassName(code);
+            CompilationUnit compilationUnit = new CompilationUnit(className, code, isATest);
+            //CompilerFactory.getCompiler(compilationUnit);
+            CompilationUnit compArray [] = new CompilationUnit[0];
+            InternalCompiler internalCompiler = new InternalCompiler(compArray);
+
+            if (internalCompiler.getTestResult().getNumberOfFailedTests() != 1) {
+                System.out.print("Too many or too less failed tests. Only one test is allowed to fail");
+
+            }else if (internalCompiler.getTestResult().getNumberOfFailedTests() == 1){
+                currentPhase = CycleEnum.CODE;
+                return currentPhase;
+
+            }else if (internalCompiler.getCompilerResult().hasCompileErrors()==false);
+                currentPhase = CycleEnum.CODE;
+                return currentPhase;
+
+        } else {
+            throw new IllegalStateException("Wrong function call");
+
+        }
+
+        // TODO: 05.07.2016 how to implement babysteps in this function?
     }
 
 
-    /**
-     * @TODO:
-     * fill the class with code
-     * are (help)methods needed?
-     * **/
 
 
 }
