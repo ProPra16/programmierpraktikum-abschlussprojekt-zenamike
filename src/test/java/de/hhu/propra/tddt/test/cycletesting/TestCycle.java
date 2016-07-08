d6f22ac117a5acb01bd728d97630f9a738e50ad5
Kevin Dreyer <schiggy69@gmail.com>
2016-07-08 19:35:15 +0200

    wrote the first cycletest and resources

---
+++ src/test/java/de/hhu/propra/tddt/test/cycletesting/TestCycle.java
@@ -0,0 +1,55 @@
+package de.hhu.propra.tddt.test.cycletesting;
+
+import de.hhu.propra.tddt.cycle.Cycle;
+import de.hhu.propra.tddt.cycle.CycleEnum;
+import de.hhu.propra.tddt.util.classnameparser.ClassNameParserException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Created by Kevin on 08.07.16.
+ */
+public class TestCycle {
+    Cycle testCycle = new Cycle();
+    CycleEnum testPhase = CycleEnum.TEST;
+    Path path =null;
+    CycleEnum testResultPhase;
+
+    @Test
+    public void testCycleTestPhase() {
+        try {
+            URL testUrl = getClass().getResource("/TestCode.java");
+            Path path = Paths.get(testUrl.getPath());
+            URL codeUrl = getClass().getResource("/NormalCode.java");
+            Path codePath = Paths.get(codeUrl.getPath());
+            String testCode = readFile(path);
+            String code = readFile(codePath);
+            testResultPhase = testCycle.testingPhase(testCode,code,testPhase);
+
+
+
+        } catch (IOException e) {
+            System.out.println(path.toAbsolutePath().toString());
+        }catch(ClassNameParserException f){
+        System.out.println(f.getMessage());
+    }
+        Assert.assertEquals(testPhase, testResultPhase);
+    }
+
+
+
+    public static String readFile(Path path)
+            throws IOException
+    {
+        final List<String> fileList = Files.readAllLines(path);
+        String filecontent = String.join("", fileList);
+        return filecontent;
+    }
+}
---
+++ src/test/resources/NormalCode.java
@@ -0,0 +1,11 @@
+public class NormalCode {
+
+
+
+        public static int awesomerechnung(){
+        int x=2;
+        int y=5;
+        int erg=x+y;
+
+        return erg;
+        }}
