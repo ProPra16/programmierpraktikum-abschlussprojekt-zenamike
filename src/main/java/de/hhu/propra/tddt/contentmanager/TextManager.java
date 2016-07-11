5b0bf801567a3cc01876efc6c994b7879fae1ee9
zebek101 <zeljkobekcic@users.noreply.github.com>
2016-07-11 19:33:05 +0200

    Changed the TextField to a TextArea

--- src/main/java/de/hhu/propra/tddt/contentmanager/TextManager.java
+++ src/main/java/de/hhu/propra/tddt/contentmanager/TextManager.java
@@ -18,7 +18,7 @@ import jdk.nashorn.internal.objects.annotations.Getter;
  */
 public class TextManager {
 
-    protected TextField textField;
+    protected TextArea textArea;
     protected String string;
     protected String phaseSave;
 
@@ -27,9 +27,9 @@ public class TextManager {
      *
      * @param textField the textfield from where the text will be pulled
      */
-    public TextManager(final TextField textField) {
-        if (textField == null) throw new IllegalArgumentException();
-        this.textField = textField;
+    public TextManager(final TextArea textArea) {
+        if (textArea == null) throw new IllegalArgumentException();
+        this.textArea = textArea;
         string = null;
     }
 
@@ -41,7 +41,7 @@ public class TextManager {
     public TextManager(final String string) {
         if (string == null) throw new IllegalArgumentException();
         this.string = string;
-        textField = null;
+        textArea = null;
     }
 
     /**
@@ -53,14 +53,14 @@ public class TextManager {
      */
     public void resetText() {
         if (phaseSave == null) phaseSave = "";
-        if (string == null) textField.setText(phaseSave);
-        if (textField == null) string = phaseSave;
+        if (string == null) textArea.setText(phaseSave);
+        if (textArea == null) string = phaseSave;
     }
 
     @Getter
     public String getText() {
-        if (string == null) return textField.getText();
-        if (textField == null) return string;
+        if (string == null) return textArea.getText();
+        if (textArea == null) return string;
         return null;
     }
 
