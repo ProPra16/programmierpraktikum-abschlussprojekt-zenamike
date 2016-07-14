23c0cb4f079dfcd2828b73c2c4a80fbc8b0cc1db
Zeljko Bekcic <zebek101@uni-duesseldorf.de>
2016-07-14 18:19:50 +0200

    Changed the last thing that went wrong :S

--- src/main/java/de/hhu/propra/tddt/compiler/CompilerManager.java
+++ src/main/java/de/hhu/propra/tddt/compiler/CompilerManager.java
@@ -1,206 +1,118 @@
 package de.hhu.propra.tddt.compiler;
 
-import de.hhu.propra.tddt.cycle.CycleEnum;
-import de.hhu.propra.tddt.util.classnameparser.ClassNameParser;
-import de.hhu.propra.tddt.util.classnameparser.ClassNameParserException;
-import vk.core.api.CompilationUnit;
-import vk.core.api.CompileError;
-import vk.core.api.CompilerFactory;
-import vk.core.api.JavaStringCompiler;
+import java.util.LinkedList;
 
-import static de.hhu.propra.tddt.compiler.CompileResults.setCycleError;
 
 /**
  * Created by schiggy on 10.07.16.
  */
 public class CompilerManager {
-    boolean firstcyle = true;
-    CycleEnum phase = CycleEnum.TEST;
-    CompileResults compResult = new CompileResults();
 
 
+    Compiler compiler = new Compiler();
+    LinkedList<String> CodeCompileMessages = new LinkedList<>();
+    LinkedList<String> TestCompileMessages = new LinkedList<>();
+    String errorMessage;
 
 
-    /**
-     * Method: compileTest
-     * <p>
-     * Task: Method that checks if the compile button is clicked, if the right phase was
-     * given to the method. If it got the right phase, the method compiles the test and if
-     * there is only one failed test or the code doesn't compile then the method sets the phase to
-     * the CODE phase.
-     *
-     * @param testCode String testCode is the whole test the user typed into the textbox
-     * @param currentPhase CycleEnum currentPhase gives information to the method with which phase
-     *                     the user is currently working. More a failsafe check for the backend
-     *                     processes than for the user.
-     *
-     * @return returns the current phase in which the cycle is at the moment
-     */
+    public CompilerManager(){
+        this.compiler = compiler;
 
-    public CycleEnum compileTest(String testCode, CycleEnum currentPhase)throws ClassNameParserException{
-        boolean isARealTest = true;
-        String testName = ClassNameParser.getClassName(testCode);
-        CompilationUnit compilationTestUnit = new CompilationUnit(testName, testCode, isARealTest);
-        JavaStringCompiler testComp;
-        testComp = CompilerFactory.getCompiler(compilationTestUnit);
-        testComp.compileAndRunTests();
-
-        if (testComp.getCompilerResult().hasCompileErrors() && firstcyle) {
-            phase = CycleEnum.CODE;
-            return currentPhase;
-        }else {setCycleError(1);}
-
-
-        if (testComp.getCompilerResult().hasCompileErrors() && !firstcyle) {
-            errorStringInit(testComp, compilationTestUnit);
-
-
-        }else if (!testComp.getCompilerResult().hasCompileErrors() && !firstcyle) {
-            compResult.setTestResults(testComp);
-
-            if (testComp.getTestResult().getNumberOfFailedTests() == 1) {
-
-                phase = CycleEnum.CODE;
-                return phase;
-
-            }else{
-                setCycleError(3);
-            }
-        }
-
-        return currentPhase;
     }
+
 
 
     /**
      * Method: compileCode
      * <p>
-     * Task: Method that checks when the compile button is clicked, if the right phase was
-     * given to the method. If it got the right phase, the method compiles the test and if
-     * there is only one failed test or the code doesn't compile then the method sets the phase to
-     * the CODE phase.
+     * Task: Method that start the compilation process, if the code produces errors,
+     * the linked list will be filled
      *
-     * @param testCode String testCode is the whole test the user typed into the textbox
-     * @param code  String code is the whole code which is going to be tested
-     * @param currentPhase CycleEnum currentPhase gives information to the method with which phase
-     *                     the user is currently working. More a failsafe check for the backend
-     *                     processes than for the user.
+     *@param code needs the testcode as a string to have something to work with.
      *
-     * @return returns the current phase in which the cycle is at the moment
+     * @return LinkedList<String> compileResults
+     *          in this order:
+     *                  compile duration
+     *                  compile errors
      */
 
+    public LinkedList<String> compileCode (String code){
+        try{
+            CodeCompileMessages = compiler.compileCode(code);
 
-
-    public CycleEnum compileCode (String code, String testCode, CycleEnum currentPhase)throws ClassNameParserException{
-        boolean isATest = false;
-        boolean isARealTest = true;
-        String className = ClassNameParser.getClassName(code);
-        String testName = ClassNameParser.getClassName(testCode);
-        CompilationUnit compilationUnit = new CompilationUnit(className, code, isATest);
-        CompilationUnit compilationTestUnit = new CompilationUnit(testName, testCode, isARealTest);
-        JavaStringCompiler testComp;
-        JavaStringCompiler codeComp;
-        testComp = CompilerFactory.getCompiler(compilationTestUnit);
-        codeComp = CompilerFactory.getCompiler(compilationUnit);
-
-        testComp.compileAndRunTests();
-        codeComp.compileAndRunTests();
-
-        if(testComp.getTestResult().getTestFailures().isEmpty()
-                && !codeComp.getCompilerResult().hasCompileErrors()
-                && testComp.getTestResult().getNumberOfIgnoredTests() == 0){
-
-            phase = CycleEnum.REFACTOR;
-            return phase;
-
-        } else if (codeComp.getCompilerResult().hasCompileErrors()) {
-            errorStringInit(codeComp, compilationUnit);
-            compResult.setCodeResults(codeComp, compilationUnit);
-
-
-        } else if (testComp.getCompilerResult().hasCompileErrors()) {
-            errorStringInit(testComp, compilationTestUnit);
-            compResult.setTestResults(testComp);
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
         }
-
-
-        return currentPhase;
-
+        return CodeCompileMessages;
     }
-
 
     /**
-     * Method: compileRefactor
+     * Method: compileTest
      * <p>
-     * Task: (does almost the same as codingPhase)
-     * Method that checks when the compile button is clicked, if the right phase was
-     * given to the method. If it got the right phase, the method compiles the test and all tests and
-     * code compiles then the user is allowed to move to complete the cycle and move to the
-     * the test phase again.
+     * Task: Method that start the compilation process for the tests, if the tests succeed
+     * or produce any errors or is uncompileable this will be put in this linked list.
      *
-     * @param testCode String testCode is the whole test the user typed into the textbox
-     * @param code  String code is the whole code which is going to be tested
-     * @param currentPhase CycleEnum currentPhase gives information to the method with which phase
-     *                     the user is currently working. More a failsafe check for the backend
-     *                     processes than for the user.
+     *@param testCode needs the testcode as a string to have something to work with.
      *
-     * @return returns the current phase in which the cycle is at the moment
+     * @return LinkedList<String> testResults
+     *              in this order:
+     *                  failed tests
+     *                  ignored tests
+     *                  successful tests
+     *                  test duration
+     *                  test fail messages
      */
 
-    public CycleEnum compileRefactor(String code, String testCode, CycleEnum currentPhase)throws ClassNameParserException{
-        boolean isATest = false;
-        boolean isARealTest = true;
-        String className = ClassNameParser.getClassName(code);
-        String testName = ClassNameParser.getClassName(testCode);
-        CompilationUnit compilationUnit = new CompilationUnit(className, code, isATest);
-        CompilationUnit compilationTestUnit = new CompilationUnit(testName, testCode, isARealTest);
-        JavaStringCompiler testComp;
-        JavaStringCompiler codeComp;
-        testComp = CompilerFactory.getCompiler(compilationTestUnit);
-        codeComp = CompilerFactory.getCompiler(compilationUnit);
-
-        testComp.compileAndRunTests();
-        codeComp.compileAndRunTests();
-
-
-        if(testComp.getTestResult().getTestFailures().isEmpty()
-                && !codeComp.getCompilerResult().hasCompileErrors()
-                && testComp.getTestResult().getNumberOfIgnoredTests() == 0){
-
-            phase = CycleEnum.TEST;
-            return phase;
-
-        } else if (codeComp.getCompilerResult().hasCompileErrors()) {
-            errorStringInit(codeComp, compilationUnit);
-            compResult.setCodeResults(codeComp, compilationUnit);
-
-
-        } else if (testComp.getCompilerResult().hasCompileErrors()){
-            errorStringInit(testComp,compilationTestUnit);
-            compResult.setTestResults(testComp);
+    public LinkedList<String> compileTest (String testCode){
+        try{
+            TestCompileMessages = compiler.compileTest(testCode);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
-
-
-        return currentPhase;
+        return TestCompileMessages;
     }
 
 
+    /****
+     * Method: setCycleError
+     * <p>
+     * Task: Method that decides which error is saved to a String so the GUI can
+     * pop an alarm box
+     *
+     * @param setError tells clearly which errors it's going to be
+     *
+     * @return void
+     */
 
 
+    public void setCycleError (int setError){  // remove or set somewhere else
 
+        switch(setError){
 
-    private void errorStringInit (JavaStringCompiler compiler, CompilationUnit cu){
-        String errorString = "";
-        for (CompileError compileError :
-                compiler.getCompilerResult().getCompilerErrorsForCompilationUnit(cu)) {
-            errorString = "Line " + compileError.getLineNumber() + ": " + compileError.getMessage() +
-                    ": \n " + compileError.getCodeLineContainingTheError() + "\n" +
-                    compileError.getMessage() + "\n";
-            compResult.setCompileErrors(errorString);
+            case 1: errorMessage = "Code is not allowed to compile here";
+                break;
+            case 2: errorMessage = "You can't change into the same phase again.";
+                break;
+            case 3: errorMessage = "You shallow not pass since something does not work, check" +
+                    "the following: ";
+                break;
+            default: errorMessage = null;
         }
 
     }
 
+    /**
+     * Method: getCycleError
+     * <p>
+     * Task: Method that just gives the errorMessage String to the GUI
+     *
+     *
+     * @return errorMessage
+     */
+
+    public String getCycleError(){
+        return errorMessage;
+    }
 
 
 }
