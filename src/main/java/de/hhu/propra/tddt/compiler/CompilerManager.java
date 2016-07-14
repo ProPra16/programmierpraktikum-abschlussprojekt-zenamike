4667020214d77c3a8ffa118c2c1788b1e6740695
Kevin Dreyer <schiggy69@gmail.com>
2016-07-14 19:43:34 +0200

    added the linkedlist which returns both result lists

--- src/main/java/de/hhu/propra/tddt/compiler/CompilerManager.java
+++ src/main/java/de/hhu/propra/tddt/compiler/CompilerManager.java
@@ -12,6 +12,7 @@ public class CompilerManager {
     Compiler compiler = new Compiler();
     LinkedList<String> CodeCompileMessages = new LinkedList<>();
     LinkedList<String> TestCompileMessages = new LinkedList<>();
+    LinkedList<LinkedList<String>> compileResultList = new LinkedList<>();
     String errorMessage;
 
 
@@ -30,20 +31,22 @@ public class CompilerManager {
      *
      *@param code needs the testcode as a string to have something to work with.
      *
-     * @return LinkedList<String> compileResults
-     *          in this order:
+     * the list will look like this
+     *            LinkedList<String> compileResults
      *                  compile duration
      *                  compile errors
+     *
+     *@return void
      */
 
-    public LinkedList<String> compileCode (String code){
+    public void compileCode (String code){
         try{
             CodeCompileMessages = compiler.compileCode(code);
 
         }catch(Exception e) {
             System.out.println(e.getMessage());
         }
-        return CodeCompileMessages;
+        addResultsIntoCompileResultList(CodeCompileMessages);
     }
 
     /**
@@ -54,22 +57,24 @@ public class CompilerManager {
      *
      *@param testCode needs the testcode as a string to have something to work with.
      *
-     * @return LinkedList<String> testResults
-     *              in this order:
+     * the list will look like this
+     *                LinkedList<String> testResults
      *                  failed tests
      *                  ignored tests
      *                  successful tests
      *                  test duration
      *                  test fail messages
+     *
+     *@return
      */
 
-    public LinkedList<String> compileTest (String testCode){
+    public void compileTest (String testCode){
         try{
             TestCompileMessages = compiler.compileTest(testCode);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
-        return TestCompileMessages;
+        addResultsIntoCompileResultList(TestCompileMessages);
     }
 
 
@@ -114,5 +119,32 @@ public class CompilerManager {
         return errorMessage;
     }
 
+
+    /**
+     * Method: addResultsIntoCompileResultList
+     * <p>
+     * adds both result list into one list
+     *
+     *
+     * @return void
+     */
+
+    private void addResultsIntoCompileResultList (LinkedList<String> list){
+        compileResultList.add(list);
+    }
+
+    /**
+     * Method: getCompileResultList
+     * <p>
+     * gives the result list to the caller
+     *
+     *
+     * @return LinkedList<LinkedList<String>> compileResultList
+     */
+
+
+    public LinkedList<LinkedList<String>> getCompileResultList(){
+        return compileResultList;
+    }
 
 }
