3332a9e474ad65c1f3bbc31a757dbae852141d9c
Zeljko Bekcic <zebek101@uni-duesseldorf.de>
2016-07-14 18:41:16 +0200

    Added a comfortable way for the GUI to handle the plugins

--- src/main/java/de/hhu/propra/tddt/cycle/CycleManager.java
+++ src/main/java/de/hhu/propra/tddt/cycle/CycleManager.java
@@ -35,9 +35,7 @@ public class CycleManager {
      *
      * @return currentPhase of the cycle
      *************************************************************************/
-
     public CycleEnum getCurrentPhase() {
-
         return cycle.getCurrentPhase();
     }
 
--- src/main/java/de/hhu/propra/tddt/plugin/PluginLoader.java
+++ src/main/java/de/hhu/propra/tddt/plugin/PluginLoader.java
@@ -1,9 +1,33 @@
 package de.hhu.propra.tddt.plugin;
 
+import de.hhu.propra.tddt.plugin.babystep.Babysteps;
+import de.hhu.propra.tddt.plugin.tracker.Tracker;
+
 /**
  * Created by zeljko On 23.06.2016
  */
+
+/*
+ * Because we had not enough time, I hardcoded this
+ */
 public class PluginLoader {
 
+    private static final Plugin babystep = new Babysteps();
+    private static final Plugin tracker = new Tracker();
 
+    private static boolean isBabystepActivated = false;
+    private static boolean isTrackerActivated = false;
+
+    public void activateBabystep(boolean activate){
+        isBabystepActivated = activate;
+    }
+
+    public void activateTracker(boolean activate){
+        isTrackerActivated = activate;
+    }
+
+    public void startAllPlugins(){
+        if(isBabystepActivated == true) babystep.start();
+        if(isTrackerActivated == true) tracker.start();
+    }
 }
